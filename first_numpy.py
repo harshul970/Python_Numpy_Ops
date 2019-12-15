23bc6f0440cd93a861ddab57e8138d6d07734600
harshul970 <harshul_j970@yahoo.com>
2019-12-16 00:08:06 +0530

    Update first_numpy.py

--- first_numpy.py
+++ first_numpy.py
@@ -16,3 +16,7 @@ npa2 = np.array(l2)
 result2 = npa1 + npa2       # Adding two numpy arrays
 
 print (result2)
+
+b = np.linspace(2,10,5)     # Creating array using linspace method. In this Example, '2' should be the starting number
+                            # '10' should be the end number, 5 should be number of partitions
+print(b)
